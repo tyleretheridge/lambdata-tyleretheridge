a6fd162990776ff63e3422ef15f366d12a9165f6
Tyler Etheridge <tylerjetheridge98@gmail.com>
2020-05-06 16:24:37 -0500

    updated file structure

--- my_lambdata/func.py
+++ my_lambdata/func.py
@@ -3,21 +3,13 @@ import pandas
 from sklearn.model_selection import train_test_split
 
 
-def add_series(X,y,name):
-    """Converts list (y) to pd.Series with name (name) 
+def add_series(X, y, name):
+    """Converts list (y) to pd.Series with name (name)
     then adds it to a dataframe (X)"""
     X = X.copy()
     series = pandas.Series(data=y, name=name)
     X[name] = series
-    
     return X
-
-
-if __name__ == "__main__":
-    df = pandas.DataFrame({'odd' : [1, 3, 5]})
-    test_list = [2, 4, 6]
-    df = add_series(df, test_list, 'even')
-    df
 
 
 def data_split(X):
@@ -26,3 +18,10 @@ def data_split(X):
     train, val = train_test_split(train, random_state=20)
 
     return train, val, test
+
+
+if __name__ == "__main__":
+    df = pandas.DataFrame({'odd': [1, 3, 5]})
+    test_list = [2, 4, 6]
+    df = add_series(df, test_list, 'even')
+    df
--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,6 +1,4 @@
 
-
-
 def enlarge(n):
     """
     Param n is a number
@@ -8,11 +6,10 @@ def enlarge(n):
     """
     return n * 100
 
+
 if __name__ == "__main__":
     # only run the code below IF this script is invoked from the command-line
     # not if it is imported from another script
     print("HELLO")
     y = int(input("Please choose a number"))
     print(y, enlarge(y))
-
-    
